89000aca5cc879fa038abb8d9eb70eef3126d5e2
shane <shanestevensxk9@gmail.com>
2022-08-05 12:46:29 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -103,12 +103,12 @@
   </div>
 </template>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+
   },
   data() {
     return {
